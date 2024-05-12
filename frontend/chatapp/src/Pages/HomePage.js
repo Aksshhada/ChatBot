83938a68640d0c2f313c69e6d00e780b8761f8b0
Akshada Kadam <akshada.kadam088@gmail.com>
2024-05-12 22:36:59 +0530

    first commit

---
+++ frontend/chatapp/src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Box } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <Container maxW='xl' centerContent>
+      <Box>
+        
+      </Box>
+    </Container>
+  )
+}
+
+export default HomePage
