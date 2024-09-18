c71e976179eea2ba2bc961c3f1e1b5d4ad7d7683
Akshada Kadam <akshada.kadam088@gmail.com>
2024-09-18 23:14:48 +0530

    Add functionalities

--- frontend/chatapp/src/components/SingleChat.js
+++ frontend/chatapp/src/components/SingleChat.js
@@ -1,78 +1,10 @@
-// import React from 'react'
-// import { ChatState } from '../Context/ChatProvider'
-// import { Box, IconButton, Text } from '@chakra-ui/react'
-// import { ArrowBackIcon } from '@chakra-ui/icons'
-// import { getSender, getSenderFull } from '../Config/ChatLogics'
-// import ProfileModal from './miscellaneous/ProfileModal'
-
-// const SingleChat = ({ fetchAgain, setFetchAgain }) => {
-
-//     const { user, selectedChat, setSelectedChat } = ChatState()
-
-//   return (
-//     <>
-//     {selectedChat ? (
-//         <>
-//         <Text
-//         fontSize={{ base: "28px", md: "30px" }}
-//         pb={"3"}
-//         px={"2"}
-//         w={"100%"}
-//         fontFamily={"Work sans"}
-//         display={"flex"}
-//         justifyContent={{ base: "space-between" }}
-//         alignItems={"center"}
-//         >
-//             <IconButton
-//             display={{ base: "flex", md: "none"}}
-//             icon={<ArrowBackIcon />}
-//             onClick={() => setSelectedChat("")}
-//             />
-//             {!selectedChat.isGroupChat ? (
-//                 <>
-//                 {getSender(user, selectedChat.users)}
-//                 <ProfileModal user={getSenderFull(user, selectedChat.users)}/>
-//                 </>
-//             ) : (
-//                 <>{selectedChat.chatName.toUpperCase()}
-//                 {/* <UpdateGroupChatModal 
-//                 fetchAgain={fetchAgain}
-//                 setFetchAgain={setFetchAgain}
-//                 /> */}
-//                 </>
-//             )}
-//         </Text>
-//         </>
-//     ) : (
-//         <Box 
-//         display={"flex"}
-//         alignItems={"center"}
-//         justifyContent={"center"}
-//         h={"100%"}>
-//         <Text fontSize={"3x1"}
-//         pb={"3"}
-//         >Click on a user to start chatting
-//         </Text>
-//         </Box>
-
-//     )}
-// </>
-//   )}
-// export default SingleChat
-
-
-
-
-
-
-
-
 import React from 'react';
 import { ChatState } from '../Context/ChatProvider';
 import { Box, IconButton, Text } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { getSender, getSenderFull } from '../Config/ChatLogics';
 import ProfileModal from './miscellaneous/ProfileModal';
+import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal';
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -113,10 +45,10 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             ) : (
               <>
                 {selectedChat.chatName.toUpperCase()}
-                {/* <UpdateGroupChatModal 
+                <UpdateGroupChatModal 
                   fetchAgain={fetchAgain}
                   setFetchAgain={setFetchAgain}
-                /> */}
+                />
               </>
             )}
           </Text>
