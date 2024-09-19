2dbc73763e980198cb91c2dfcaab398fd2fc3627
Akshada Kadam <akshada.kadam088@gmail.com>
2024-09-19 22:13:40 +0530

    backend funtionalities

--- backend/servermain.js
+++ backend/servermain.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./Config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
